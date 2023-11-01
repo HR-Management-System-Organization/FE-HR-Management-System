861f4d3dde33d4d9a941533b13f74b797e006f16
Alperen Sertoğlu <127945529+alperensertoglu@users.noreply.github.com>
2023-11-01 16:35:34 +0300

    Delete hr-mangement-panel directory

--- hr-mangement-panel/src/components/AuthComp/index.js
+++
@@ -1,9 +0,0 @@
-import useAuth from "../../hooks/auth"
-
-const AuthComp = ({ authKey, children }) => {
-  const checkAuth = useAuth(authKey);
-
-  return checkAuth && children;
-};
-
-export default AuthComp;
